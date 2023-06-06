73108b9dd123f086313056bf728e6a8fe1aed38c
Gabriel Vasconcelos <97545157+gabriel-vasconcelos-ft@users.noreply.github.com>
2023-06-06 10:23:11 -0300

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -12,6 +11,16 @@ fig, ax = plt.subplots(figsize=(7,5))
 sns.histplot(base, x='Popularity', kde=True, color='b')
 plt.xlabel('Popularidade')
 plt.ylabel('Contagem')
-plt.title('Gráfico de Contagem da Popularidade')
-plt.xticks(rotation=30)
+plt.title('Histograma de Contagem da Popularidade')
 st.pyplot(fig)
+
+
+pop = base.head(12)['Name'].value_counts()
+fig, ax = plt.subplots(figsize=(10,10))
+sns.barplot(x=pop.index, y=pop.values)
+plt.xlabel('Artista')
+plt.ylabel('Contagem')
+plt.title('Contagem de Artistas')
+plt.xticks(rotation=60)
+st.pyplot(fig)
+
